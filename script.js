a084ecff1f889c11eb9c0db4cb492039400fd1fe
carpophorus <markomrkonjic9@gmail.com>
2019-09-20 14:46:22 +0200

    adjusted for hiding the api

--- script.js
+++ script.js
@@ -29,8 +29,9 @@
 
     var lang = srp;
 
-    let apiRoot = 'https://api.parking-pilot.com/';
-    let apiKey = '?api_key=4B160CD3C5BD53B146571C440F11D1CB';
+    apiRoot = 'http://10.0.66.2:7752/api/';
+    //let apiRoot = 'https://api.parking-pilot.com/';
+    //let apiKey = '?api_key=4B160CD3C5BD53B146571C440F11D1CB';
     let leftCam = 'http://10.0.16.46/jpg/image.jpg';
     let rightCam = 'http://10.0.16.45/jpg/image.jpg';
 
@@ -103,7 +104,8 @@
 
     P.getFullStatus = function(idSensor, idSpace) {
         sendGetRequest(
-            apiRoot + 'parkingspaces/' + idSensor + '/status' + apiKey,
+            /*apiRoot + 'parkingspaces/' + idSensor + '/status' + apiKey*/
+            apiRoot + 'status' + '?sensorID=' + idSensor,
             function(response, status) {
                 $(".jconfirm").remove();
                 var date = new Date(response.last_change * 1000);
@@ -125,6 +127,26 @@
             },
             true, null
         );
+    };
+
+    P.showDetails = function(time) {
+        $(".jconfirm").remove();
+        var date = new Date(time * 1000);
+        var dateString = '' + date.getFullYear() + '.' + ('0' + (date.getMonth() + 1)).slice(-2) + '.' + ('0' + date.getDate()).slice(-2) + '. ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2);
+        $.confirm({
+            title: lang.spot + idSpace,
+            content: `<br><img id="snapshot" src="` + (idSpace == 1 || idSpace == 2 || idSpace == 10 || idSpace == 12 || idSpace == 16 || idSpace == 17 ? rightCam : leftCam) + `" width="500" height="auto" onclick="if ($('#snapshot').attr('width') == 500) $('#snapshot').attr('width', '` + window.innerWidth + `'); else $('#snapshot').attr('width', 500);" onload="setTimeout(function() {$('#snapshot').attr('src', '` + (idSpace == 1 || idSpace == 2 || idSpace == 10 || idSpace == 12 || idSpace == 16 || idSpace == 17 ? rightCam : leftCam) + `');}, 2000);"><br>` + /*lang.companyPhone*/ `<br>` + (response.occupied === true ? lang.occupied : lang.available) + lang.since + dateString,
+            theme: 'supervan',
+            backgroundDismiss: 'true',
+            buttons: {
+                ok: {
+                    text: 'ОК',
+                    btnClass: '',
+                    keys: ['enter'],
+                    action: function() {}
+                }
+            }
+        });
     };
 
     processResponse = function(response) {
@@ -173,45 +195,46 @@
         mostRecentResponse = response;
     };
 
-    retrieveStatuses = function(idLot) {
+    retrieveStatuses = function(/*idLot*/) {
         sendGetRequest(
-            apiRoot + 'parkinglots/' + idLot + '/parkingspaces' + apiKey,
+            /*apiRoot + 'parkinglots/' + idLot + '/parkingspaces' + apiKey*/
+            apiRoot + 'all_statuses',
             function(response, status) {
-                setTimeout(function() { retrieveStatuses(idLot); }, 2000);
-                console.log('ping');
+                setTimeout(function() { retrieveStatuses(/*idLot*/); }, 4000);
                 var array = response.sort(function(a, b) { return a.xml_id - b.xml_id; });
                 processResponse(array);
                 var html = '';
                 for(var i = 0; i < 9; i++)
-                    html += '<div class="v ' + (array[i].occupied === true ? 'occupied' : 'free') + (array[i].occupied != array[i].occupied_preliminary ? ' transitioning' : '') + '" onclick="$P.getFullStatus(' + array[i].id + ', ' + array[i].xml_id + ');">' + array[i].xml_id + '</div>';
+                    html += '<div class="v ' + (array[i].occupied === true ? 'occupied' : 'free') + (array[i].occupied != array[i].occupied_preliminary ? ' transitioning' : '') + '" onclick="$P.showDetails(' + array[i].last_change /* + '" onclick="$P.getFullStatus(' + array[i].id + ', ' + array[i].xml_id */ + ');">' + array[i].xml_id + '</div>';
                 html += '<br>';
                 for(var s = 0; s < 9; s++)
                     html += '<div class="v-space"></div>';
                 html += '<br>';
                 for(i = 16; i > 11; i--)
-                    html += '<div class="v ' + (array[i].occupied === true ? 'occupied' : 'free') + (array[i].occupied != array[i].occupied_preliminary ? ' transitioning' : '') + '" onclick="$P.getFullStatus(' + array[i].id + ', ' + array[i].xml_id + ');">' + array[i].xml_id + '</div>';
+                    html += '<div class="v ' + (array[i].occupied === true ? 'occupied' : 'free') + (array[i].occupied != array[i].occupied_preliminary ? ' transitioning' : '') + '" onclick="$P.showDetails(' + array[i].last_change /* + '" onclick="$P.getFullStatus(' + array[i].id + ', ' + array[i].xml_id */ + ');">' + array[i].xml_id + '</div>';
                 for(s = 0; s < 2; s++)
                     html += '<div class="v-space"></div>';
                 for(i = 10; i > 8; i--)
-                    html += '<div class="v ' + (array[i].occupied === true ? 'occupied' : 'free') + (array[i].occupied != array[i].occupied_preliminary ? ' transitioning' : '') + '" onclick="$P.getFullStatus(' + array[i].id + ', ' + array[i].xml_id + ');">' + array[i].xml_id + '</div>';
+                    html += '<div class="v ' + (array[i].occupied === true ? 'occupied' : 'free') + (array[i].occupied != array[i].occupied_preliminary ? ' transitioning' : '') + '" onclick="$P.showDetails(' + array[i].last_change /* + '" onclick="$P.getFullStatus(' + array[i].id + ', ' + array[i].xml_id */ + ');">' + array[i].xml_id + '</div>';
                 html += '<br><div class="square-space"></div>';
                 for(i = 17; i < 19; i++)
-                    html += '<div class="h ' + (array[i].occupied === true ? 'occupied' : 'free') + (array[i].occupied != array[i].occupied_preliminary ? ' transitioning' : '') + '" onclick="$P.getFullStatus(' + array[i].id + ', ' + array[i].xml_id + ');">' + array[i].xml_id + '</div>';
+                    html += '<div class="h ' + (array[i].occupied === true ? 'occupied' : 'free') + (array[i].occupied != array[i].occupied_preliminary ? ' transitioning' : '') + '" onclick="$P.showDetails(' + array[i].last_change /* + '" onclick="$P.getFullStatus(' + array[i].id + ', ' + array[i].xml_id */ + ');">' + array[i].xml_id + '</div>';
                 html += '<div class="h-space"></div>';
-                html += '<div class="h ' + (array[11].occupied === true ? 'occupied' : 'free') + (array[11].occupied != array[11].occupied_preliminary ? ' transitioning' : '') + '" onclick="$P.getFullStatus(' + array[11].id + ', ' + array[11].xml_id + ');">' + array[11].xml_id + '</div>';
+                html += '<div class="h ' + (array[11].occupied === true ? 'occupied' : 'free') + (array[11].occupied != array[11].occupied_preliminary ? ' transitioning' : '') + '" onclick="$P.showDetails(' + array[11].last_change /* + '" onclick="$P.getFullStatus(' + array[11].id + ', ' + array[11].xml_id */ + ');">' + array[11].xml_id + '</div>';
                 $('.parking').html(html);
             },
             true, null
         );
     };
 
-    sendGetRequest(
+    /*sendGetRequest(
         apiRoot + 'parkinglots' + apiKey,
         function(response, status) {
             retrieveStatuses(response[0].id);
         },
         true, null
-    );
+    );*/
+    retrieveStatuses();
 
     P.langChanged = function () {
         if ($('.language input:checked').val() == 'srp')
